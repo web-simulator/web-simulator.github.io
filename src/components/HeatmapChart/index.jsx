c218bbb7a847e6f89665551d627bf730d7829700
Mickaell-Oliveira <mickaell.oliveira@estudante.ufjf.br>
2026-07-07 19:42:09 -0300

    fix: corrige memoria que não era desalocada no chart 2d

--- src/components/HeatmapChart/index.jsx
+++ src/components/HeatmapChart/index.jsx
@@ -125,7 +125,7 @@ const HeatmapChart = ({ data, nCols, minValue = 0, maxValue = 1, onPointClick, f
     
     glRef.current = {
         gl, program, positionBuffer, texCoordBuffer, dataTexture, fibrosisTexture,
-        fibUploaded: false, prevFibMap: null
+        fibUploaded: false, prevFibMap: null, dataUploaded: false
     };
 
     return () => {
@@ -148,6 +148,7 @@ const HeatmapChart = ({ data, nCols, minValue = 0, maxValue = 1, onPointClick, f
         canvasRef.current.height = nRows;
         gl.viewport(0, 0, nCols, nRows);
         glRef.current.fibUploaded = false;
+        glRef.current.dataUploaded = false;
     }
     
     gl.useProgram(program);
@@ -183,11 +184,17 @@ const HeatmapChart = ({ data, nCols, minValue = 0, maxValue = 1, onPointClick, f
     gl.activeTexture(gl.TEXTURE0);
     gl.bindTexture(gl.TEXTURE_2D, dataTexture);
     gl.pixelStorei(gl.UNPACK_ALIGNMENT, 1);
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.R32F, nCols, nRows, 0, gl.RED, gl.FLOAT, data);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    
+    if (!glRef.current.dataUploaded) {
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.R32F, nCols, nRows, 0, gl.RED, gl.FLOAT, data);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+        glRef.current.dataUploaded = true;
+    } else {
+        gl.texSubImage2D(gl.TEXTURE_2D, 0, 0, 0, nCols, nRows, gl.RED, gl.FLOAT, data);
+    }
     
     gl.uniform1i(gl.getUniformLocation(program, "u_data"), 0);
     gl.uniform1i(gl.getUniformLocation(program, "u_fibrosis"), 1);
